9efad09e38f66698d169925d8884f9f354610bc2
AamriGuleria <92876738+AamriGuleria@users.noreply.github.com>
2024-09-19 22:50:27 +0530

    connecting backend to frontend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const server=http.createServer(app)
 const io=new Server(server,
     {
         cors:{
-            origin:"http://localhost:3000",
+            origin:"https://chat-application-frontend-9oek.onrender.com/",
             methods:["GET","POST"]
         }
     }
